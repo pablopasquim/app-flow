bd9c7052509688a7d32e24092c3eb8f0f9603c5c
pablopasquim <142011551+briansantoss@users.noreply.github.com>
2024-08-29 09:35:42 -0300

    Add: .gitignore

--- app/src/main/java/com/example/pabloapp/MainActivity.kt
+++ app/src/main/java/com/example/pabloapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.pabloapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pabloapp.ui.theme.PabloAppTheme
@@ -23,7 +26,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Mensagem(){
-    Text(text = "Bom dia!")
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Aula 08")
+        Text(text = "Bom dia!")
+    }
 }
 
 @Preview (showBackground = true)
