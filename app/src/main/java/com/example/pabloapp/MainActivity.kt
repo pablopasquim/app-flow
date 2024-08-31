92fbbb0133447c37469c464bf0acb27f6ea43497
pablopasquim <pablo.pasquim7@gmail.com>
2024-08-31 17:44:43 -0300

    ✨Feat: Adicioanando subtração, multplicação e divisão de valores

--- app/src/main/java/com/example/pabloapp/MainActivity.kt
+++ app/src/main/java/com/example/pabloapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.pabloapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,16 +26,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pabloapp.ui.theme.PabloAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Somar()
+                Calculadora()
             }
         }
     }
@@ -121,7 +129,7 @@ fun ExemploTextField(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Somar(){
+fun Calculadora(){
 
     var n1 by remember { mutableStateOf("") }
     var n2 by remember { mutableStateOf("") }
@@ -157,25 +165,88 @@ fun Somar(){
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
 
-            var valor1 = n1.toIntOrNull() ?: 0
-            var valor2 = n2.toIntOrNull() ?: 0
+        ) {
 
-            result = valor1 + valor2
+            Button(onClick = {
 
-        }) {
-            Text(text = "Somar")
+                var valor1 = n1.toIntOrNull() ?: 0
+                var valor2 = n2.toIntOrNull() ?: 0
+
+                result = valor1 + valor2
+
+            },
+                ) {
+                Text(text = "Somar")
+            }
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Button(onClick = {
+
+                var valor1 = n1.toIntOrNull() ?: 0
+                var valor2 = n2.toIntOrNull() ?: 0
+
+                result = valor1 - valor2
+
+            }) {
+                Text(text = "Subtrair")
+            }
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Button(onClick = {
+
+                var valor1 = n1.toIntOrNull() ?: 0
+                var valor2 = n2.toIntOrNull() ?: 0
+
+                if(valor1 == 0 || valor2 == 0){
+                    result = 0
+                } else
+                    result = valor1 * valor2
+
+            }) {
+                Text(text = "Multiplicar")
+            }
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Button(onClick = {
+
+                var valor1 = n1.toIntOrNull() ?: 0
+                var valor2 = n2.toIntOrNull() ?: 0
+
+                if(valor1 == 0 || valor2 == 0){
+                    result = 0
+                } else
+                    result = valor1 / valor2
+
+            }) {
+                Text(text = "Dividir")
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
         }
 
-        Text(text = "Resultado = $result")
+        Text(
+            text = "Resultado = $result",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(16.dp)
+            )
     }
+
 }
 
 @Preview (showBackground = true)
 @Composable
 fun Call(){
-    Somar()
+    Calculadora()
 }
 
 
